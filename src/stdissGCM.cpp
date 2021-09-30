2f270dd0e37a4be6e9a25e4ae2bcf30b4f94c576
lenarddome <lenarddome@protonmail.ch>
2021-09-30 11:35:09 +0100

    finish dissGCM (both original and modified)

--- src/stdissGCM.cpp
+++ src/stdissGCM.cpp
@@ -5,7 +5,7 @@
 using namespace Rcpp;
 using namespace arma;
 
-// Equations correspond to O'Bryan et al. (2018)
+// Equations correspond to Stewart and Miron (2007)
 // Equation 1
 mat distanceMatrix(double r, rowvec attention, mat exemplars, rowvec input) {
   mat difference = input - exemplars.each_row();
@@ -17,7 +17,6 @@ mat distanceMatrix(double r, rowvec attention, mat exemplars, rowvec input) {
 }
 
 // Assumption: muting exemplars with non-matching features
-// TODO: only compare where input is 1
 vec mute(mat exemplars, rowvec input) {
   uvec indices = find(input == 1);
   mat difference = input - exemplars.each_row();
@@ -34,6 +33,7 @@ vec mute(mat exemplars, rowvec input) {
 }
 
 // Equation 2
+// Exponential transformation of distances
 mat exponential_similarity(double c, int outcomes, colvec distances) {
   colvec power = distances * c;
   colvec similarity = zeros(outcomes);
@@ -44,7 +44,7 @@ mat exponential_similarity(double c, int outcomes, colvec distances) {
 }
 
 // Equation 3
-// returns evidence for each category
+// Returns evidence for each category.
 mat evidence(colvec t, double s, int outcomes, vec mute,
              colvec categories, colvec similarity) {
   colvec similar = s * (similarity % t);
@@ -62,31 +62,76 @@ mat evidence(colvec t, double s, int outcomes, vec mute,
   return evidence;
 }
 
-// Equation 4
-vec respond(double b, colvec evidence, double outcomes) {
-  double denominator = sum(evidence) + (outcomes * b);
-  mat out = (evidence + b) / denominator;
+// Equation 4 (O'Bryan et al., 2018)
+// Background-noise decision network
+vec respond_noise(double base, colvec evidence, double outcomes) {
+  double denominator = sum(evidence) + (outcomes * base);
+  mat out = (evidence + base) / denominator;
   return out;
 }
 
+// Equation 4 (Stewart and Mintor, 2007)
+// Store number of trials since exemplar appeared
+vec trial_count(colvec distances, colvec count) {
+  uvec close = find(distances > 0);
+  for (uword i = 0; i < close.n_elem; i++) {
+    count[close[i]] += 1;
+  }
+  if(any(distances == 0)) {
+    uvec current = find(distances == 0);
+    count[current[0]] = 0;
+  }
+  return count;
+}
+
+// Exemplar memory decay
+vec decay(colvec n, double theta, colvec distances) {
+  uvec close = find(distances > 0);
+  colvec term = n * theta;
+  colvec result = zeros(distances.n_elem);
+  for (uword i = 0; i < close.n_elem; i++) {
+    result[close[i]] -= std::exp(-term[close[i]]);
+  }
+  // cap values at 0
+  return result;
+}
+
+// Equation (5)
+// Biased decision rule network
+vec respond_bias(colvec beta, colvec evidence, double gamma) {
+  double denominator = sum(pow(beta % evidence, gamma));
+  colvec nominator = pow(beta % evidence, gamma);
+  mat out = nominator / denominator;
+  return out;
+}
+
+
 // [[Rcpp::export]]
-Rcpp::List stdissGCM(List st, arma::mat test, bool xtdo = false) {
+Rcpp::List stdissGCM(List st, arma::mat test,
+                     std::string dec = "NOISE",
+                     bool exemplar_decay = true,
+                     bool exemplar_mute = true,
+                     bool xtdo = false) {
 
   // declare initial state of the model
-  double    r = as<double>(st["r"]); // Euclidean - distance metric
-  double    c = as<double>(st["c"]); // specificity constant
-  double    s = as<double>(st["s"]); // similarity/dissimilarity weight
-  double    b = as<double>(st["b"]); // baseline level of similarity
-  colvec    t = as<colvec>(st["t"]); // exemplar-specific memory strength
+  double    r = as<double>(st["r"]);    // Euclidean - distance metric
+  double    c = as<double>(st["c"]);    // generalization/specificity constant
+  double    s = as<double>(st["s"]);    // similarity/dissimilarity weight
+  double    theta = as<double>(st["theta"]); // decay parameter
+  double    gamma = as<double>(st["gamma"]); // decision scaling constant
+  colvec    beta = as<colvec>(st["beta"]);    // category bias vector
+  double    base = as<double>(st["b"]);    // baseline level of similarity
   rowvec    attention = as<rowvec>(st["attentional_weights"]);
-  int       outcomes = as<int>(st["outcomes"]);
-  double    num_out = as<double>(st["outcomes"]);
+  int       outcomes = as<int>(st["outcomes"]);  // number of outcomes (integer)
+  double    num_out = as<double>(st["outcomes"]); // number of outcomes (double)
   int       colskip = as<int>(st["colskip"]);
   mat       exemplars = as<mat>(st["exemplars"]);
   mat       pure_exemplars = exemplars.cols(0, exemplars.n_cols - 2);
   colvec    pure_categories = exemplars.col(exemplars.n_cols - 1);
 
+
   // declare variables used for the implementation
+  colvec count; count = zeros(exemplars.n_rows);
   rowvec trial(test.n_cols);
   rowvec input(pure_exemplars.n_cols);
   uword n_trials = test.n_rows;
@@ -95,23 +140,43 @@ Rcpp::List stdissGCM(List st, arma::mat test, bool xtdo = false) {
   vec probabilities; probabilities = zeros(outcomes);
   vec evidence_all; evidence_all = zeros(outcomes);
   vec muted; muted = zeros(exemplars.n_rows);
+  // conditional parameters
+  colvec    t = as<colvec>(st["t"]);    // exemplar-specific memory strength
+  if(exemplar_decay) t = zeros(exemplars.n_rows);
+  mat       memory_decay(n_trials, exemplars.n_rows);
+
   // declare output of main function
   mat evidence_out(n_trials, outcomes);
   mat similarity_out(n_trials, outcomes);
   mat prob(n_trials, outcomes);
+
   List outFIN;
 
   for (uword i = 0; i < n_trials; i++) {
     input = test.row(i);
     distances = distanceMatrix(r, attention, pure_exemplars, input);
     similarity = exponential_similarity(c, outcomes, distances);
-    muted = mute(pure_exemplars, input);
-    evidence_all = evidence(t, s, outcomes, muted, pure_categories, similarity);
-    probabilities = respond(b, evidence_all, num_out);
+    if(exemplar_mute) {
+      muted = mute(pure_exemplars, input);
+    } else {
+      muted = ones(exemplars.n_rows);
+    }
+    evidence_all = evidence(t, s, outcomes, muted,
+                            pure_categories, similarity);
+    if(exemplar_decay) {
+      count = trial_count(distances, count);
+      t = decay(count, theta, distances);
+    }
+    if(dec == "NOISE") {
+      probabilities = respond_noise(base, evidence_all, num_out);
+    } else if(dec == "BIAS") {
+      probabilities = respond_bias(beta, evidence_all, gamma);
+    }
     // record outputs
     similarity_out.row(i) = evidence_all.t().as_row();
     evidence_out.row(i) = similarity.t().as_row();
     prob.row(i) = probabilities.as_row();
+    if(exemplar_decay) memory_decay.row(i) = t.t().as_row();
   }
   return Rcpp::List::create(Rcpp::Named("Evidence") = evidence_out,
                             Rcpp::Named("Similarity") = similarity_out,
