80031e1480d2e0ca648558090d090f2f60c9d062
lenarddome <lenarddome@protonmail.ch>
2021-11-16 15:56:57 +0000

    start programming model 4 from scratch with armadillo

--- tmp/model4test.R
+++ tmp/model4test.R
@@ -1,22 +1,23 @@
 library(catlearn)
 library(Rcpp)
+library(data.table)
 sourceCpp("src/slpModel4.cpp")
 
 params = c(2.87, 2.48, 4.42, 4.42, .222, 1.13, .401)
-tr <- krus96train(blocks = 15, subjs = 56, ctxt = TRUE, seed = 1)
+tr <- krus96train(blocks = 15, subjs = 1, ctxt = FALSE, seed = 1)
+tr <- data.table(tr)
+print(tr)
+
 
 ## Set state
-nFeat = 7
-nCat = 4
-w_in_out <- matrix(0, nCat, nFeat)
-
-st <- list(nFeat = nFeat, nCat = nCat,
-           phi = params[3], c = params[1], P = params[2],
-           l_gain = params[4], l_weight = params[5], l_ex = params[6],
-           sigma = c(rep(1,6), params[7]),
-           iterations = 10,
-           exemplars = exemplars,
-           w_exemplars = w_exemplars,
-           w_in_out = w_in_out)
+st <- list(P = params[1],     ## attentional normalization
+           phi = params[2],   ## response consistency
+           delta = params[3], ## learning rate parameter
+           mu = params[4],    ## attentional learning rate parameter
+           colskip = 1,
+           outcomes = 4,
+           w = NULL,
+           eta = NULL       ## intial salience of cues
+       )
 
 ## Run simulation
